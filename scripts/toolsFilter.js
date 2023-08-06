186a95166c06982c266f272bc39ad1bf90abf98a
Mariia Drobina <m.drobinawork@mail.ru>
2023-08-07 00:39:51 +0200

    1

--- scripts/toolsFilter.js
+++ scripts/toolsFilter.js
@@ -1,51 +1,52 @@
-function ToolsFilter(entity, path){
-
-  this.filter = function(button){
-    filterTools($(button)[0].innerText)
+class ToolsFilter {
+  constructor(entity) {
+    this.entity = entity;
+    this.filter = function (button) {
+      this.#filterTools($(button)[0].innerText);
+    };
   }
   
-  function getListsOfTags() {
+  #getListsOfTags() {
     const $tags = $('.tags');
-    const $toolsNames = $(`.${entity}__description__text`).find('h3');
+    const $toolsNames = $(`.${this.entity}__description__text`).find('h3');
     const tagsList = {};
-    for(let i = 0; i < $tags.length; i++){
+    for (let i = 0; i < $tags.length; i++) {
       const currentTool = $toolsNames[i].innerText;
-      tagsList[currentTool] = []
+      tagsList[currentTool] = [];
       for (let tag of $tags[i].children) {
-        
-        tagsList[currentTool].push(tag.innerText)
+
+        tagsList[currentTool].push(tag.innerText);
       };
     }
     return tagsList;
   }
-
-  function filterTools(category){
-    resetToolsDisplay();
-    const listsOfTags = getListsOfTags();
-    const $toolsNames = $(`.${entity}__description__text`).find('h3');
+  #filterTools(category) {
+    this.#resetToolsDisplay();
+    if (category == 'All') {
+      this.#resetToolsDisplay();
+      return
+    }
+    const listsOfTags = this.#getListsOfTags();
+    const $toolsNames = $(`.${this.entity}__description__text`).find('h3');
     for (let i = 0; i < $toolsNames.length; ++i) {
       const currentToolName = $toolsNames[i].innerText;
       let $currentTool;
-      if(entity == 'tool'){
+      if (this.entity == 'tool') {
         $currentTool = $($toolsNames[i].parentElement.parentElement.parentElement);
-      } else if(entity == 'job'){
-        $currentTool = $($toolsNames[i].parentElement.parentElement.parentElement.parentElement)
+      } else if (this.entity == 'job') {
+        $currentTool = $($toolsNames[i].parentElement.parentElement.parentElement.parentElement);
       }
-      console.log(currentToolName)
       if (listsOfTags[currentToolName].includes(category)) {
-        $currentTool.css('display','flex')
+        $currentTool.css('display', 'flex');
         $currentTool.css('opacity', '1');
         $currentTool.css('animation', 'none');
-      } else if(category == 'All') {
-        resetToolsDisplay();
       } else {
-        $currentTool.css('display','none')
+        $currentTool.css('display', 'none');
       }
     }
   }
-
-  function resetToolsDisplay() {
-    const $tools = $(`.${entity}`);
+  #resetToolsDisplay() {
+    const $tools = $(`.${this.entity}`);
     $tools.css('display', 'flex');
     $tools.css('opacity', '1');
   }
