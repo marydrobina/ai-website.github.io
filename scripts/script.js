b2ae71b34b4214f9d6c9e8867bd022aae340a4e9
Mariia Drobina <m.drobinawork@mail.ru>
2023-08-10 11:21:11 +0200

    Added Read more to conferences

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,49 @@
-const particles = new Particles();;
-particles.createBubbles();
-
-function openMenu() {
-  let nav = document.querySelector("#main-navigation");
-  nav.classList.toggle('responsive')
+import Particles from './particles'
+class App{
+  constructor(){
+    const particles = new Particles();
+    particles.createBubbles();
+  }
 }
-$(window).on('scroll', () => {
-  const $win = $(window);
-  const windowHeight = $win.height();
 
-  $('.toAnim').each(function() {
-    const $this = $(this);
-    const elementOffset = $this.offset().top;
-    const scrollPosition = $win.scrollTop();
 
-    // Calculate the position relative to the bottom of the viewport
-    const distanceFromBottom = scrollPosition + windowHeight;
+// const particles = new Particles();
+// particles.createBubbles();
 
-    // Check if the element is within 70% of the viewport height from the bottom
-    if (elementOffset < distanceFromBottom - windowHeight * 0.2) {
-      $this.addClass('anim');
-    }
-  });
-});
+// function openMenu() {
+//   let nav = document.querySelector("#main-navigation");
+//   nav.classList.toggle('responsive')
+// }
+// $(window).on('scroll', () => {
+//   const $win = $(window);
+//   const windowHeight = $win.height();
 
-$('.filter').on('click', function()
-      { 
-        if (this.parentElement.parentElement.id == 'tools-section'){
-          const filterTools = new ToolsFilter('tool');
-          filterTools.filter(this)
-        } else if (this.parentElement.parentElement.id == 'jobs-section'){
-            const filterJobs = new ToolsFilter('job');
-            filterJobs.filter(this)
-        }
-      }
-    )
+//   $('.toAnim').each(function() {
+//     const $this = $(this);
+//     const elementOffset = $this.offset().top;
+//     const scrollPosition = $win.scrollTop();
+
+//     // Calculate the position relative to the bottom of the viewport
+//     const distanceFromBottom = scrollPosition + windowHeight;
+
+//     // Check if the element is within 70% of the viewport height from the bottom
+//     if (elementOffset < distanceFromBottom - windowHeight * 0.2) {
+//       $this.addClass('anim');
+//     }
+//   });
+// });
+
+// $('.filter').on('click', function()
+//       { 
+//         if (this.parentElement.parentElement.id == 'tools-section'){
+//           const filterTools = new ToolsFilter('tool');
+//           filterTools.filter(this)
+//         } else if (this.parentElement.parentElement.id == 'jobs-section'){
+//             const filterJobs = new ToolsFilter('job');
+//             filterJobs.filter(this)
+//         }
+//       }
+//     )
 
 
 
